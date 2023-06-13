4b530497f6bc17434d88947b0484db6b99d6aaec
EverEmpty <fairel95@gmail.com>
2023-06-13 20:25:00 +0300

    done

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,7 +1,6 @@
 package web.dao;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import web.model.User;
 
 import javax.persistence.EntityManager;
@@ -19,25 +18,21 @@ public class UserDaoImpl implements UserDao{
         return entityManager.createQuery("from User", User.class).getResultList();
     }
     @Override
-    @Transactional
     public void saveUser(User user) {
         entityManager.persist(user);
     }
 
     @Override
-    @Transactional
     public void updateUser(User user) {
         entityManager.merge(user);
     }
 
     @Override
-    @Transactional
     public void deleteUser(long ID) {
         entityManager.remove(entityManager.find(User.class, ID));
     }
 
     @Override
-    @Transactional
     public User getUserById(long ID) {
         return entityManager.createQuery("from User where id = :id", User.class)
                 .setParameter("id", ID).getSingleResult();
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,12 +1,13 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
 import java.util.List;
 
-@Component
+@Service
 public class UserServiceImpl implements UserService{
 
     private UserDao userDao;
@@ -15,26 +16,31 @@ public class UserServiceImpl implements UserService{
         this.userDao = userDao;
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<User> userList(){
         return userDao.userList();
     }
 
+    @Transactional
     @Override
     public void saveUser(User user){
         userDao.saveUser(user);
     }
 
+    @Transactional
     @Override
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
+    @Transactional
     @Override
     public void deleteUser(long ID) {
         userDao.deleteUser(ID);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public User getUserById(long ID) {
         return userDao.getUserById(ID);
